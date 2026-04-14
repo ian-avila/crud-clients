889e83904ff731a7fa57ed3ac00f1104dc37c49c
ian-avila <ian.avg07@gmail.com>
2026-04-14 20:51:30 -0300

    Create Client entity and map with JPA

---
+++ src/main/java/com/ian_avila/crud_client/entities/Client.java
@@ -0,0 +1,94 @@
+package com.ian_avila.crud_client.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String cpf;
+
+    private Double income;
+
+    private LocalDate birth_date;
+    private Integer children;
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate birth_date, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birth_date = birth_date;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDate birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Client client)) return false;
+
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
